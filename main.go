7a762e958e6bf2b59176fc59a2d4e2fed9d40ac6
gopherwxf <68725032@qq.com>
2023-01-29 19:57:50 +0800

    增加缓存组件

--- main.go
+++ main.go
@@ -9,12 +9,15 @@ func main() {
 
 	r := gin.New()
 	r.Handle("GET", "/news/:id", func(ctx *gin.Context) {
+		//1. 从对象池 获取新闻缓存 对象
 		newsCache := lib.NewsCache()
 		defer lib.ReleaseNewsCache(newsCache)
 
+		//2. 获取参数，设置DBGetter
 		newsID := ctx.Param("id")
 		newsCache.DBGetter = lib.NewsDBGetter(newsID)
 
+		//3. 取缓存输出，如果没有则调用上面的DBGetter
 		//ctx.Header("Content-type", "application/json")
 		//ctx.String(200, res.(string))
 		newsModel := lib.NewNewsModel()
